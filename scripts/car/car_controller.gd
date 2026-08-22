c11f630153742b8b0e1c084bceaf89966fe26ebf
Tiago Xavier <braga.tiagoxavier@gmail.com>
2026-08-22 17:08:24 -0300

    refactor: convert car controller to CharacterBody3D with move_and_slide

--- scripts/car/car_controller.gd
+++ scripts/car/car_controller.gd
@@ -1,19 +1,19 @@
-class_name Car
-extends RigidBody3D
+class_name CarController
+extends CharacterBody3D
 
-@export var acceleration: float = 10.0
-@export var max_speed: float = 25.0
 @export var lane_distance: float = 2.5
 @export var lane_limit: int = 1
 @export var lane_speed: float = 10.0
+@export var forward_speed: float = 5.0
 @export var delay_input: float = 0.3
 
 var _current_lane: int = 0
 var _can_switch_lane: bool = true
 
 func _physics_process(delta: float) -> void:
-	_drive_forward()
 	_track_lane(delta)
+	_forward_move()
+	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("move_left"):
@@ -21,13 +21,6 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event.is_action_pressed("move_right"):
 		_change_lane(1)
 
-
-func _drive_forward() -> void:
-	var forward := -global_basis.z
-	if linear_velocity.dot(forward) >= max_speed:
-		return
-
-	apply_central_force(forward * acceleration * mass)
 
 func _change_lane(direction: int) -> void:
 	if not _can_switch_lane:
@@ -40,8 +33,10 @@ func _change_lane(direction: int) -> void:
 	await get_tree().create_timer(delay_input).timeout
 	_can_switch_lane = true
 
-
 func _track_lane(delta: float) -> void:
 	var target_x := _current_lane * lane_distance
-	linear_velocity.x = clampf(
+	velocity.x = clampf(
 		(target_x - global_position.x) / delta, -lane_speed, lane_speed)
+
+func _forward_move() -> void:
+	velocity.z = -forward_speed
